28ac1a0a52921e5453997038be07c1277b787c3a
gustbrad <36966517+gustbrad@users.noreply.github.com>
2018-08-04 11:26:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-// Dependencies
-// =============================================================
->>>>>>> d78367866c6981a1458e9fe4b88f59c02c9098fd
-=======
 
 // Dependencies
 // =============================================================
 
->>>>>>> fbe7b760eb035e946015c465f6d8a4dcdfea800f
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var table= [];
 var wait= [];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-var app = express();
-var PORT = 2121;
 
-var reservations= [];
 
-var waitList= [];
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-   });
-=======
->>>>>>> fbe7b760eb035e946015c465f6d8a4dcdfea800f
-=======
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -49,8 +28,4 @@ app.use(bodyParser.json());
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-<<<<<<< HEAD
->>>>>>> d78367866c6981a1458e9fe4b88f59c02c9098fd
-=======
 
->>>>>>> fbe7b760eb035e946015c465f6d8a4dcdfea800f
